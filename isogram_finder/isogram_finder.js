ffc28d72a4cb161846cd3e0468e19be6c416ace6
Fidelma <fidelma.beagan@gmail.com>
2019-07-31 18:03:56 +0100

    isogram finder complete

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,24 @@
 const IsogramFinder = function (word) {
-
+  this.word = word.toLowerCase().split('');
 }
 
-IsogramFinder.prototype.isIsogram = function () {
+// IsogramFinder.prototype.isIsogram = function () {
+//   let lettersSoFar = [];
+//   return this.word.forEach((letter) => {
+//       if (!lettersSoFar.includes(letter)){
+//       lettersSoFar.push(letter);
+//     }
+//    let success = this.word.every((letter) => {
+//      return this.phrase.indexOf(letter) !== -1;
+//    });
+//   });
+//   return success;
+// }
 
+IsogramFinder.prototype.isIsogram = function () {
+ return this.word.every((letter, index) => {
+   return this.word.indexOf(letter) == index;
+ })
 }
 
 module.exports = IsogramFinder;
