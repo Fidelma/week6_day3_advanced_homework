7e33af3bfacabdb7bc3205a5eea12f3271f2e5ed
Fidelma <fidelma.beagan@gmail.com>
2019-07-31 16:55:22 +0100

    pangramFinder complete

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,21 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase.toLowerCase().split('');
 }
 
-PangramFinder.prototype.isPangram = function () {
 
+// PangramFinder.prototype.isPangram = function () {
+//   var success = this.alphabet.every((val) => {
+//     return this.phrase.indexOf(val) !== -1;
+// });
+//   return success;
+// }
+
+PangramFinder.prototype.isPangram = function () {
+  var success = this.alphabet.every((val) => {
+    return this.phrase.includes(val);
+});
+  return success;
 }
 
 module.exports = PangramFinder;
